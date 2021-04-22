afbeab2d69fe2e4ddc629d9b3414449b108063ad
Diogo Gomes <dgomes@graphnode.com>
2021-04-22 12:55:02 +0100

    feat: added link for github repository

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,16 @@
 import Draggable from '../Draggable';
 
+import { ReactComponent as GitHubIcon } from '../../assets/github.svg';
+
 import styles from './App.module.css';
 
 const App = () => (
   <div className={styles.App}>
     <Draggable />
+
+    <a href={process.env.REACT_APP_GITHUB_URL} target="_blank" rel="noreferrer" className={styles.GitHubLink}>
+      <GitHubIcon />
+    </a>
   </div>
 );
 
