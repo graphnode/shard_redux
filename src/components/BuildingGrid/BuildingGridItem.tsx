ef263247bf4ca7d25316617b68264421843501b5
Diogo Gomes <dgomes@graphnode.com>
2021-05-21 18:39:36 +0100

    Added snaping to building grid.

--- src/components/BuildingGrid/BuildingGridItem.tsx
+++ src/components/BuildingGrid/BuildingGridItem.tsx
@@ -7,23 +7,28 @@ import * as S from './BuildingGridItem.styles';
 export interface BuildingGridItemProps {}
 
 const BuildingGridItem : React.FC<BuildingGridItemProps> = () => {
-  const [styles, api] = useSpring(() => ({ x: 0, y: 0 }));
+  const [{ x, y }, api] = useSpring(() => ({ x: 64, y: 64 }));
 
-  const bind = useDrag(({ event, last, offset: [ox, oy] }) => {
+  const bind = useDrag(({ event, last, movement: [mx, my] }) => {
     event.stopPropagation();
 
-    let x = ox - 16, y = oy - 16;
+    let x = mx, y = my;
 
     if (last) {
       x = Math.round(x / 32) * 32;
       y = Math.round(y / 32) * 32;
     }
 
-    api.start({ x, y, config: { tension: 250 } });
+    api.start({
+      x, y,
+      config: { tension: 250 },
+    });
+  }, {
+    initial: () => [Math.round(x.get() / 32) * 32, Math.round(y.get() / 32) * 32],
   });
 
   return (
-    <S.Container style={styles} {...bind()} />
+    <S.Container {...bind()} style={{ x, y }} />
   );
 };
 
